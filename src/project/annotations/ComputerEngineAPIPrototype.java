f97f0e4149a2bba0ebc3cf34fdb4cd2d9fb05ced
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-13 20:08:40 -0500

    Update ComputerEngineAPIPrototype.java

--- src/project/annotations/ComputerEngineAPIPrototype.java
+++ src/project/annotations/ComputerEngineAPIPrototype.java
@@ -2,52 +2,16 @@ package project.annotations;
 
 public class ComputerEngineAPIPrototype implements ComputerEngineAPI {
 	
-    private String source;
-    private String delimiter = ","; // Default delimiter
-    private String destination;
-
-    //set the source
     @NetworkAPIPrototype
-    public void setSource(String source) {
-        // Logic to set source (file, networked drive, etc.)
-    	this.source = source;
-    }
+    public void prototypeMethod(ComputerEngineAPI api) {
+        api.setSource("example_source.txt");
+        api.setDelimiter(",");
+        api.setDestination("output_file.csv");
 
-    //set the delimiters
-    @NetworkAPIPrototype
-    public void setDelimiter(String delimiter) {
-        // Logic to set delimiter (default or custom)
-    	if (delimiter == null || delimiter.isEmpty()) {
-            this.delimiter = ",";  // Use default if no delimiter is provided
-        } else {
-            this.delimiter = delimiter;
-        }
-    	
-    }
-
-    //set the destination
-    @NetworkAPIPrototype
-    public void setDestination(String destination) {
-        // Logic to set destination (file, networked drive, etc.)
-    	this.destination = destination;
-    }
-
-    //get the source from user
-    @NetworkAPIPrototype
-    public String getSource() {
-        return source;
-    }
-
-    //get the delimiter from user input
-    @NetworkAPIPrototype
-    public String getDelimiter() {
-        return delimiter;
-    }
-
-    //get the destination from user
-    @NetworkAPIPrototype
-    public String getDestination() {
-        return destination;
+        System.out.println("Prototype Simulation:");
+        System.out.println("Source: " + api.getSource());
+        System.out.println("Delimiter: " + api.getDelimiter());
+        System.out.println("Destination: " + api.getDestination());
     }
     
 }
