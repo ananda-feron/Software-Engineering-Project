c1acb94d3db9d3fa49db98109c30fc83ae27639b
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-13 19:56:15 -0500

    Update ComputerEngine.java to different name

--- src/project/annotations/ComputerEngine.java
+++ src/project/annotations/ComputerEngine.java
@@ -1,7 +1,7 @@
 package project.annotations;
 
 @NetworkAPI
-public interface ComputerEngine {
+public interface ComputerEngineAPI {
     void setSource(String source);  // Set the source (file, networked drive, etc.)
     void setDelimiter(String delimiter);  // Set the delimiter (can be default or custom)
     void setDestination(String destination);  // Set the destination (file, networked drive, etc.)
