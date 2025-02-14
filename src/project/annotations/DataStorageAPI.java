0982390fd204f60f101f544bde5a01391d03dafa
Ananda Feron <153582208+ananda-feron@users.noreply.github.com>
2025-02-13 20:14:56 -0500

    Update DataStorageAPI.java
    updated the abstract methods, updated the package, and removed the imports

--- src/project/annotations/DataStorageAPI.java
+++ src/project/annotations/DataStorageAPI.java
@@ -1,15 +1,9 @@
-package api;
-
-import model.DataReadRequest;
-import model.DataReadResponse;
-import model.DataWriteRequest;
-import model.DataWriteResponse;
-import annotations.ProcessAPI;
+package project.annotations;
 
 @ProcessAPI
 public interface DataStorageAPI {
-    // reads in data requests (from: ...)
-    DataReadResponse readData(DataReadRequest request);
-    // writes out data requests (to: ...)
-    DataWriteResponse writeData(DataWriteRequest request);
+    // reads in data requests ("from: ...")
+    void readData();
+    // writes out data requests ("to: ...")
+    void writeData();
 }
