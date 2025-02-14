347267db28a649bf1532f173ebe44f962cc8bdfd
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-13 20:07:53 -0500

    Update ComputerEngineAPI.java

--- src/project/annotations/ComputerEngineAPI.java
+++ src/project/annotations/ComputerEngineAPI.java
@@ -5,4 +5,8 @@ public interface ComputerEngineAPI {
     void setSource(String source);  // Set the source (file, networked drive, etc.)
     void setDelimiter(String delimiter);  // Set the delimiter (can be default or custom)
     void setDestination(String destination);  // Set the destination (file, networked drive, etc.)
+
+    String getSource();
+    String getDelimiter();
+    String getDestination();
 }
