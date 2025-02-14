0f222c4a6e03b708525fc37985af4533eeed69bf
Ananda Feron <153582208+ananda-feron@users.noreply.github.com>
2025-02-13 19:51:58 -0500

    Update and Rename src/api/DataStorageAPI.java to src/project/annotations/DataStorageAPI.java
    added comments and renamed path

--- src/project/annotations/DataStorageAPI.java
+++ src/project/annotations/DataStorageAPI.java
@@ -8,6 +8,8 @@ import annotations.ProcessAPI;
 
 @ProcessAPI
 public interface DataStorageAPI {
+    // reads in data requests (from: ...)
     DataReadResponse readData(DataReadRequest request);
+    // writes out data requests (to: ...)
     DataWriteResponse writeData(DataWriteRequest request);
 }
