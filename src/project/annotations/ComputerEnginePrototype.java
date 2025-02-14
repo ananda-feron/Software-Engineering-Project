14ae3a54cf2116a8df6414a02a99884811bc94fd
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-13 19:51:27 -0500

    Update ComputerEnginePrototype.java
    added comments to set and get methods

--- src/project/annotations/ComputerEnginePrototype.java
+++ src/project/annotations/ComputerEnginePrototype.java
@@ -5,13 +5,15 @@ public class ComputerEnginePrototype implements ComputerEngine {
     private String source;
     private String delimiter = ","; // Default delimiter
     private String destination;
-	
+
+    //set the source
     @NetworkAPIPrototype
     public void setSource(String source) {
         // Logic to set source (file, networked drive, etc.)
     	this.source = source;
     }
-    
+
+    //set the delimiters
     @NetworkAPIPrototype
     public void setDelimiter(String delimiter) {
         // Logic to set delimiter (default or custom)
@@ -23,22 +25,26 @@ public class ComputerEnginePrototype implements ComputerEngine {
     	
     }
 
+    //set the destination
     @NetworkAPIPrototype
     public void setDestination(String destination) {
         // Logic to set destination (file, networked drive, etc.)
     	this.destnation = destination;
     }
-    
+
+    //get the source from user
     @NetworkAPIPrototype
     public String getSource() {
         return source;
     }
 
+    //get the delimiter from user input
     @NetworkAPIPrototype
     public String getDelimiter() {
         return delimiter;
     }
 
+    //get the destination from user
     @NetworkAPIPrototype
     public String getDestination() {
         return destination;
