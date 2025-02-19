da9fd987278563af5c21311fd8e759886ad1765a
aidan <aidanod3@gmail.com>
2025-02-19 12:22:19 -0500

    Added EmptyImplementationNetworkAPI

---
+++ src/main/java/usercomputeapi/EmptyImplementationNetworkAPI.java
@@ -0,0 +1,24 @@
+package usercomputeapi;
+
+public class EmptyImplementationNetworkAPI implements ComputeEngine {
+
+    @Override
+    public InputSourceResponse requestInput(InputSourceRequest inputSourceRequest) {
+        return null;
+    }
+
+    @Override
+    public DelimiterResponse requestDelimiter(DelimiterRequest delimiterRequest) {
+        return null;
+    }
+
+    @Override
+    public OutputDestinationResponse requestOutputDestination(OutputDestinationRequest outputDestinationRequest) {
+        return null;
+    }
+
+    @Override
+    public SendDataResponse sendData(InputSource inputSource, String delimiter, OutputDestination output) {
+        return null;
+    }
+}
