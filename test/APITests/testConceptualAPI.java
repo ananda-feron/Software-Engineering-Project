ddfe72f02cdacaae2decfcdcd6a017b582b19b9a
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-20 00:12:16 -0500

    Smoke Tests added
    They are not functional though

--- test/APITests/testConceptualAPI.java
+++ test/APITests/testConceptualAPI.java
@@ -1,18 +1,32 @@
 package APITests;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+
 import org.junit.jupiter.api.Test;
 
 public class testConceptualAPI {
 
     @Test
     public void testReceiveTaskResponse(){
+        InputStream mockInputStream = mockito.mock(InputStream.class);
 
+        when(mockInputStream.receiveTask(any(ReceiveTaskRequest.class))).thenReturn(new ReceiveTaskResponse())
+
+        PrototypeTaskHandler testComponent = newPrototypeTaskHandler(mockInputStream);
+
+        verify(mockInputStream).receiveTask(any(ReceiveTaskRequest.class));
     }
 
     @Test
     public void testSendTaskResponse(){
+        InputStream mockInputStream = mockito.mock(InputStream.class);
 
+        when(mockInputStream.sendTask(any(SendTaskResponse.class))).thenReturn(new SendTaskResponse());
+
+        PrototypeTaskHandler testComponent = newPrototypeTaskHandler(mockInputStream);
+
+        verify(mockInputStream).sendTask(any(SendTaskRequest.class));
     }
 
 }
--- test/APITests/testProcessAPI.java
+++ test/APITests/testProcessAPI.java
@@ -1,18 +1,32 @@
 package APITests;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+
 import org.junit.jupiter.api.Test;
 
 public class testProcessAPI {
 
     @Test
     public void testReadResponse(){
+        Datastore mockDatastore = mockito.mock(Datastore.class);
 
+        when(Datastore.readfromInput(any(ReadInputRequest.class))).thenReturn(new readResponse());
+
+        PrototypeTaskHandler testComponent = newPrototypeTaskHandler(mockDataStore);
+
+        verify(mockDataStore).readFromInput(any(ReadInputRequest.class));
     }
 
     @Test
     public void testWriteResponse(){
+        Datastore mockDatastore = mockito.mock(Datastore.class);
 
+        when(Datastore.writeToOutput(any(OutputDestinationRequest.class))).thenReturn(new WriteResponse());
+
+        PrototypeTaskHandler testComponent = newPrototypeTaskHandler(mockDataStore);
+
+        verify(mockDataStore).readFromInput(any(OutputDestinationRequest.class));
     }
 
 }
