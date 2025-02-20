adc6d767d575d3671a151a1150e00995f8441f18
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-20 18:35:44 -0500

    Re-coded the ConecptualAPI Test. (And also added gitignore files for intellij)

---
+++ test/APITests/TestConceptualAPI.java
@@ -0,0 +1,43 @@
+package APITests;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import computetaskhandlerapi.InputStream;
+import computetaskhandlerapi.ReceiveTaskRequest;
+import computetaskhandlerapi.ReceiveTaskResponse;
+import computetaskhandlerapi.SendTaskRequest;
+import computetaskhandlerapi.SendTaskResponse;
+import computetaskhandlerapi.PrototypeTaskHandler;
+
+public class TestConceptualAPI {
+
+    @Test
+    public void testPrototypeReceiveTask() {
+        InputStream mockInputStream = mock(InputStream.class);
+
+        ReceiveTaskResponse mockReceiveTaskResponse = mock(ReceiveTaskResponse.class);
+
+        when(mockInputStream.receiveTask(any(ReceiveTaskRequest.class))).thenReturn(mockReceiveTaskResponse);
+
+        PrototypeTaskHandler testComponent = new PrototypeTaskHandler();
+
+        testComponent.prototype(mockInputStream);
+
+        verify(mockInputStream).receiveTask(any(ReceiveTaskRequest.class));
+    }
+
+    @Test
+    public void testPrototypeSendTask() {
+        InputStream mockInputStream = mock(InputStream.class);
+
+        SendTaskResponse mockSendTaskResponse = mock(SendTaskResponse.class);
+
+        when(mockInputStream.sendTask(any(SendTaskRequest.class))).thenReturn(mockSendTaskResponse);
+
+        PrototypeTaskHandler testComponent = new PrototypeTaskHandler();
+
+        testComponent.prototype(mockInputStream);
+
+        verify(mockInputStream).sendTask(any(SendTaskRequest.class));
+    }
+}
