b1affbbf91463ab70f52a41a5e3b1f1367c73b15
aidan <aidanod3@gmail.com>
2025-02-21 11:07:19 -0500

    fixed in memory data store
    removed * import

--- test/smoke/InMemoryDataStore.java
+++ test/smoke/InMemoryDataStore.java
@@ -1,8 +1,14 @@
 package smoke;
 
-import processapi.*;
+import processapi.DataStore;
+import processapi.ReadInputResponse;
+import processapi.ReadInputRequest;
+import processapi.WriteOutputRequest;
+import processapi.WriteOutputResponse;
 
-
+//import processapi.*;
+//TODO NOT DONE. I will fix this at some point today.
+//
 public class InMemoryDataStore implements DataStore {
     @Override
     public ReadInputResponse readFromInput(ReadInputRequest readInputRequest) {
@@ -13,6 +19,7 @@ public class InMemoryDataStore implements DataStore {
     public WriteOutputResponse writeToOutput(WriteOutputRequest sendOutputRequest) {
         return null;
     }
+
 }
 
 
