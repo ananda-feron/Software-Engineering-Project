15d75a1e8fd025b0bda4e83443820955a1720320
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-24 18:38:02 -0500

    Implemented Collatz Sequence Code

--- src/main/java/apis/ComputeEngineImpl.java
+++ src/main/java/apis/ComputeEngineImpl.java
@@ -5,9 +5,31 @@ public class ComputeEngineImpl implements ComputeEngineAPI {
     @Override
     public String compute(int value) {
 
-        //insert actual code to compute the collatz sequence!
+        //I think we should rename "compute" to "computeCollatzSequence"
+        //Also I know this won't pass the test. Adjust the code here so it
+        //passes it.
 
-        return null;
+        /*
+        if (value <= 0) {
+            throw new IllegalArgumentException("Value must be a positive integer.");
+        }
 
+        (Maybe we need an exception?)
+        */
+
+        StringBuilder builder = new StringBuilder();
+
+        while(value != 1) {
+            builder.append(value).append(" -> ");
+            if(value % 2 == 0) {
+                value = value / 2;
+            } else {
+                value = 3 * value + 1;
+            }
+        }
+
+        builder.append("1");
+
+        return builder.toString();
     }
 }
