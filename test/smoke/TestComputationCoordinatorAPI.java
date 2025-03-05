39cbe1e0e0c59ee509d80682fff9ec2992a7e578
aidan <aidanod3@gmail.com>
2025-03-04 21:32:52 -0500

    Update TestComputationCoordinatorAPI.java

--- test/smoke/TestComputationCoordinatorAPI.java
+++ test/smoke/TestComputationCoordinatorAPI.java
@@ -60,7 +60,7 @@ public class TestComputationCoordinatorAPI {
 
         ComputeRequest computeRequest = null;
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {computationCoordinator.compute(computeRequest);});
+        Assertions.assertThrows(IllegalArgumentException.class, () -> computationCoordinator.compute(computeRequest));
 
     }
 }
