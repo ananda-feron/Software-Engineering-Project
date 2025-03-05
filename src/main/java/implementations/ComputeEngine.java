66bdeccbebd1cebcacc854b26c16b68baed86cd1
BananaCotton57 <johnnyngo772@gmail.com>
2025-03-04 21:38:14 -0500

    Added Runtime Exception to Engine

--- src/main/java/implementations/ComputeEngine.java
+++ src/main/java/implementations/ComputeEngine.java
@@ -7,23 +7,29 @@ public class ComputeEngine implements ComputeEngineAPI {
     @Override
     public String compute(int value) {
 
-        if(value <= 0){
-            throw new IllegalArgumentException("Error: value most be positive. Your input: " + value);
-        }
+        try {
 
-        StringBuilder builder = new StringBuilder();
-
-        builder.append(value);
-
-        while(value != 1) {
-            if(value % 2 == 0) {
-                value = value / 2;
-            } else {
-                value = 3 * value + 1;
+            if (value <= 0) {
+                throw new IllegalArgumentException("Error: value most be positive. Your input: " + value);
             }
-            builder.append(",").append(value);
-        }
 
-        return builder.toString();
+            StringBuilder builder = new StringBuilder();
+
+            builder.append(value);
+
+            while (value != 1) {
+                if (value % 2 == 0) {
+                    value = value / 2;
+                } else {
+                    value = 3 * value + 1;
+                }
+                builder.append(",").append(value);
+            }
+
+            return builder.toString();
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
