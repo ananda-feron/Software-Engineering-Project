e205456a8790b19e7e09c4da4e10b24f5be2224b
BananaCotton57 <johnnyngo772@gmail.com>
2025-03-31 14:12:19 -0400

    Update TestMultiUser.java

--- test/TestMultiUser.java
+++ test/TestMultiUser.java
@@ -7,9 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import apis.ComputationCoordinatorAPI;
-import apis.ComputeRequest;
-import apis.ComputeResult;
+import apis.*;
 import implementations.ComputationCoordinator;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,12 +18,14 @@ public class TestMultiUser {
 	// TODO 1: change the type of this variable to the name you're using for your @NetworkAPI
 	// interface
 	private ComputationCoordinator coordinator;
+	private ComputeEngineAPI computeEngine;
+	private DataStoreAPI dataStore;
 	
 	@BeforeEach
 	public void initializeComputeEngine() {
 		//TODO 2: create an instance of the implementation of your @NetworkAPI; this is the component
 		// that the user will make requests to
-		ComputationCoordinator instance = new ComputationCoordinator();
+		ComputationCoordinator instance = new ComputationCoordinator(computeEngine, dataStore);
         // Store it in the 'coordinator' instance variable
 		coordinator = instance;
 	}
