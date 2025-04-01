9627a8de65f89de694a00f38c607e4c877bf1beb
BananaCotton57 <johnnyngo772@gmail.com>
2025-03-31 22:32:18 -0400

    Update Testuser.java
    Trying to do TODO 4

--- test/TestUser.java
+++ test/TestUser.java
@@ -1,5 +1,9 @@
 import apis.ComputationCoordinatorAPI;
+import apis.ComputeRequest;
 import implementations.ComputationCoordinator;
+import implementations.ComputeResult;
+import implementations.FileInput;
+import implementations.FileOutput;
 
 import java.io.File;
 
@@ -20,6 +24,18 @@ public class TestUser {
 		
 		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
 		// run the compute job specified by inputPath, outputPath, and delimiter
+		// Wrap paths in InputConfig and OutputConfig objects
+		FileInput inputConfig = new FileInput(inputPath);
+		FileOutput outputConfig = new FileOutput(outputPath);
+
+		// Create ComputeRequest with proper input and output
+		ComputeRequest request = new ComputeRequest(inputConfig, outputConfig, delimiter);
+
+		// Run computation
+		ComputeResult result = coordinator.compute(request);
+
+		// Print result
+		System.out.println("Computation Result: " + result);
 	}
 
 }
