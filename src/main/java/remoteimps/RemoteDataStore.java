85e522903f26fa2b84d09f74d50fb450ac9d56c5
aidan <aidanod3@gmail.com>
2025-04-15 01:32:27 -0400

    Fleshed out RemoteComputationCoordinator, CollatzServer, and ComputeEngineClient
    finishes step 1 and 2 of the assignment.  step 3 still isnt done, there is no usage of protocol buffers between the data storage component and the compute engine.

---
+++ src/main/java/remoteimps/RemoteDataStore.java
@@ -0,0 +1,77 @@
+package remoteimps;
+
+import apis.DataStoreReadResult;
+import apis.InputConfig;
+import apis.OutputConfig;
+import apis.WriteResult;
+import implementations.DataStoreReadResultImpl;
+import implementations.WriteResultImpl;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RemoteDataStore {
+
+    public DataStoreReadResult read(InputConfig input) {
+
+        try {
+            if (input == null) {
+                return new DataStoreReadResultImpl(DataStoreReadResult.Status.FAILURE, null, "Input is invalid.");
+            }
+
+
+            List<Integer> integerList = new ArrayList<>();
+
+            try {
+                File inputFile = (File) input.getInput();
+                Scanner scanner = new Scanner(inputFile);
+                while (scanner.hasNextLine()) {
+                    String[] values = scanner.nextLine().split(",");
+                    //assumed to be .csv file
+                    for (String value : values) {
+                        try { //(validation)
+                            integerList.add(Integer.parseInt(value));
+                        } catch (NumberFormatException numberFormatException) {
+                            System.err.println("skipping invalid number: " + value);
+                        }
+                    }
+                }
+            } catch (IOException ioException) {
+                return new DataStoreReadResultImpl(DataStoreReadResult.Status.FAILURE, null, "Error reading file: " + ioException.getMessage());
+            }
+
+            return new DataStoreReadResultImpl(DataStoreReadResult.Status.SUCCESS, integerList, "Successfully read file.");
+        } catch (Exception e){
+            return new DataStoreReadResultImpl(DataStoreReadResult.Status.FAILURE, null, "Unexpected runtime error: " + e.getMessage());
+        }
+    }
+
+    public WriteResult appendSingleResult(OutputConfig output, String result, char delimiter) {
+        try {
+            if (output == null) {
+                return new WriteResultImpl(WriteResult.WriteResultStatus.FAILURE, "Output is invalid.");
+            }
+            try {
+                File outputFile = (File) output.getOutput();
+                if (!outputFile.exists()) {
+                    System.out.println("Output file " + outputFile + " does not exist. creating it...");
+                }
+                FileWriter writer = new FileWriter(outputFile, true);
+                writer.write(result + delimiter + "\n");
+                writer.close();
+
+            } catch (IOException ioException) {
+                return new WriteResultImpl(WriteResult.WriteResultStatus.FAILURE, "Error writing to file: " + ioException.getMessage());
+            }
+            return new WriteResultImpl(WriteResult.WriteResultStatus.SUCCESS, "Success writing to file.");
+
+        } catch (Exception e){
+            return new WriteResultImpl(WriteResult.WriteResultStatus.FAILURE, "Unexpected runtime error: " + e.getMessage());
+        }
+
+    }
+}
