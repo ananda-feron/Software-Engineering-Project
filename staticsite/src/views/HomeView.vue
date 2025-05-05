bdaccdf04ace304434d11f328bde89cac457e90e
BananaCotton57 <johnnyngo772@gmail.com>
2025-05-05 11:41:06 -0400

    radio buttons

--- staticsite/src/views/HomeView.vue
+++ staticsite/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const pick = ref("File")
 </script>
 
 <template>
   <main>
+    <h1>Welcome to the Collatz Sequence Generator!</h1>
+    <h2>Please pick one of the two options below</h2>
     <ul>
-    <li><input type="radio" v-model="pick" :value="first" /></li>
-    <li><input type="radio" v-model="pick" :value="second" /></li>
+    <li ><input type="radio" v-model="pick" value="File" /> File </li>
+    <li ><input type="radio" v-model="pick" value="List" /> List </li>
     </ul>
   </main>
 </template>
